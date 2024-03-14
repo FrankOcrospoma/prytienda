24e8ca69d96412f04293724fbe21759968b135b5
Frank <frankocrospomaugaz@gmail.com>
2024-03-14 17:32:30 -0500

    first commit

--- dependencias.bat
+++ dependencias.bat
@@ -1,19 +1,19 @@
-@REM @echo off
+@echo off
 
-@REM rem Instalar dependencias de Composer
-@REM call composer install
+rem Instalar dependencias de Composer
+call composer install
 
-@REM rem Instalar dependencias de npm
-@REM call npm install
+rem Instalar dependencias de npm
+call npm install
 
-@REM rem Copiar el archivo .env.example
-@REM call copy .env.example .env
+rem Copiar el archivo .env.example
+call copy .env.example .env
 
-@REM rem Generar la clave de aplicación
-@REM call php artisan key:generate
+rem Generar la clave de aplicación
+call php artisan key:generate
 
-@REM rem Ejecutar las migraciones de la base de datos
-@REM call php artisan migrate
+rem Ejecutar las migraciones de la base de datos
+call php artisan migrate
 
 rem Abrir una nueva consola y ejecutar el servidor de desarrollo
 start cmd /k "npm run dev"
